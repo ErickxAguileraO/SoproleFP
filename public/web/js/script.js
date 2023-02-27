d2e36a715920587ad0df11e4d0a51c571f3a741b
unknown <bcoloma@aeurus.cl>
2023-02-27 10:43:31 -0300

    dev

--- public/web/js/script.js
+++ public/web/js/script.js
@@ -147,7 +147,7 @@ $('.carruselRecetas').slick({
     dots: false,
     infinite: true,
     speed: 300,
-    slidesToShow: 4,
+    slidesToShow: 2,
     slidesToScroll: 1,
     responsive: [
       {
