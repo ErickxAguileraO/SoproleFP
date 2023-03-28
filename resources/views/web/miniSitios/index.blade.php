84b94fb8b311c7e5cf031f82579fab993d3236ec
Joel <jsalazar@aeurus.cl>
2023-03-28 09:53:51 -0300

    Se corrigieron los textos del home

--- resources/views/web/miniSitios/index.blade.php
+++ resources/views/web/miniSitios/index.blade.php
@@ -19,7 +19,7 @@
 
         @if (count($tags) > 0)
             <div class="flex-tu-negocio">
-                <h2>¿De qué es tu negocio?</h2>
+                <h2>¿Qué tipo de preparaciones desarrollas en tu negocio? </h2>
                 <br>
                 <div class="tu-negocio">
                     @foreach ($tags as $tag)
@@ -36,8 +36,8 @@
             <section class="seccion-home removeSection">
                 <h2>Mira nuestros productos</h2>
                 <br>
-                <p class="sub-titulo">Como líderes en innovación e investigación de Mercado, constantemente estamos creando
-                    nuevos usos lácteos, perfectos para cada propósito</p>
+                <p class="sub-titulo">Nos apasiona entregar productos de calidad. Por ello, estamos enfocados en innovar y
+                    desarrollar productos acordes a las necesidades de tu negocio.</p>
                 <div class="cuadros-info cuadros-row-4">
                     @foreach ($productos as $producto)
                         <a href="/productos/detalle/{{ $producto->pro_url }}" class="cuadros-info-n">
@@ -57,12 +57,14 @@
                 <div class="seccion-home">
                     <h2>Academia Food Professionals</h2>
                     <br>
-                    <p class="sub-titulo">Nos apasiona ayudar a nuestros clientes, logrando la optimización de sus recursos,
-                        haciendo más eficiente sus procesos, alcanzando una mayor calidad y expertíz en su negocio</p>
+                    <p class="sub-titulo">Nos apasiona potenciar el desarrollo de tu negocio gastronómico. Por ello, a
+                        través de nuestra academia, te brindamos herramientas que buscan potenciar tu pastelería, pizzería,
+                        restaurante, banquetera o cafetería</p>
                     <div class="cuadros-info cuadros-row-4">
 
                         @foreach ($academias as $academia)
-                            <a href="{{route('web.academia.detalle', $academia->aca_id).'-'.$academia->aca_url }}" class="cuadros-info-n">
+                            <a href="{{ route('web.academia.detalle', $academia->aca_id) . '-' . $academia->aca_url }}"
+                                class="cuadros-info-n">
                                 <div class="img"><img src="{{ asset($academia->aca_imagen) }}" alt="">
                                 </div>
                                 <div class="texto">
@@ -72,7 +74,8 @@
                             </a>
                         @endforeach
                     </div>
-                    <a href="{{route('web.academia.index').'?segmentoId[0]='.$segmento->seg_id}}" class="boton bg-red">Ver más cursos</a>
+                    <a href="{{ route('web.academia.index') . '?segmentoId[0]=' . $segmento->seg_id }}"
+                        class="boton bg-red">Ver más cursos</a>
                 </div>
             </section>
         @endif
@@ -83,11 +86,12 @@
             <section class="seccion-home removeSection">
                 <h2>Mira estas recetas</h2>
                 <br>
-                <p class="sub-titulo">Como líderes en innovación e investigación de Mercado, constantemente estamos creando
-                    nuevos usos lácteos, perfectos para cada propósito</p>
+                <p class="sub-titulo">Nos apasiona ayudarte a crecer, y con estas recetas, podrás expandir tu menú y
+                    descubrir nuevas técnicas para crear sonrisas en quienes disfruten de tus preparaciones. </p>
                 <div class="cuadros-info cuadros-row-3">
                     @foreach ($recetas as $receta)
-                        <a href="{{route('web.receta.detalle', $receta->rec_id).'-'.$receta->rec_url }}" class="cuadros-info-n">
+                        <a href="{{ route('web.receta.detalle', $receta->rec_id) . '-' . $receta->rec_url }}"
+                            class="cuadros-info-n">
                             <div class="img"><img src="{{ asset($receta->rec_imagen) }}" alt="">
                             </div>
                             <div class="texto">
@@ -104,11 +108,12 @@
             <section class="seccion-home removeSection">
                 <h2>Tendencias y Noticias</h2>
                 <br>
-                <p class="sub-titulo">Como líderes en innovación e investigación de Mercado, constantemente estamos creando
-                    nuevos usos lácteos, perfectos para cada propósito</p>
+                <p class="sub-titulo">Nos apasiona mantenerte informado, y esta sección, encontrarás las últimas noticias
+                    sobre capacitaciones, eventos, tendencias y todo lo relacionado al mundo gastronómico.</p>
                 <div class="cuadros-info cuadros-row-3">
                     @foreach ($noticias as $noticia)
-                        <a href="{{route('web.noticia.detalle', $noticia->not_id).'-'.$noticia->not_url }}" class="cuadros-info-n">
+                        <a href="{{ route('web.noticia.detalle', $noticia->not_id) . '-' . $noticia->not_url }}"
+                            class="cuadros-info-n">
                             <div class="img"><img src="{{ asset($noticia->imagenListado->ino_imagen) }}" alt="">
                             </div>
                             <div class="texto">
