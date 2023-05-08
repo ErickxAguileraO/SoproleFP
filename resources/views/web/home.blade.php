5288789b7b634dad729b039bff25a4a2fa3e8417
unknown <bcoloma@aeurus.cl>
2023-05-08 17:44:12 -0400

    Modicaciones Home 5

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -151,7 +151,7 @@
                 <div class="conocenos-txt">
                     <h3>Conócenos</h3>
                     @php
-                        echo $conocenos->edi_contenido;
+                        echo $conocenos->edi_bajada;
                     @endphp
                     <a href="/conocenos" class="boton-conocenos">Ver más sobre nosotros</a>
                 </div>
